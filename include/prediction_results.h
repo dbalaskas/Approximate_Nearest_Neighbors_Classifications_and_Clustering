9c39e252e92048e89ac55980d021d871622b8242
Andreas <andreas.giannoutsos@gmail.com>
2020-10-09 12:57:19 +0300

    status

--- include/prediction_results.h
+++ include/prediction_results.h
@@ -1,9 +1,30 @@
 #ifndef PRED_RES_H
 #define PRED_RES_H
 
-class Results {
-    private:
-    public:
-};
+#include <vector>
+#include <stdio.h>
+#include <cstdlib>
+#include <iostream>
+
+typedef struct PointIndex{
+
+    PointIndex() {}
+    PointIndex(int intex, double dist) : intex(intex), dist(dist) {}
+    int intex;
+    double dist;
+
+} PointIndex;
+
+typedef struct Results{
+
+    Results() {}
+    Results(int size) {this->resultList.reserve(size);}
+    int numOfResults;
+    std::vector<PointIndex> resultList;
+    double time;
+
+} Results;
+
+
 
 #endif
